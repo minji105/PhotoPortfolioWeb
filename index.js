00b8aa95d2b6a679891a7083b10e863bdce76c73
minji105 <ajhjmj9@naver.com>
2024-04-24 01:55:41 +0900

    menu page

--- index.js
+++ index.js
@@ -1,62 +1,115 @@
-// 클릭 시 화면 전환
-const contactButton = document.getElementById('contact-button');
+// 클릭 시 클립보드 복사 & 텍스트 바꿈
+// function copyClipboard() {
+//     const text = document.getElementById('text').textContent;
+//     const textarea = document.createElement('textarea');
+//     textarea.textContent = text;
+
+//     document.body.append(textarea);
+//     textarea.select();
+//     document.execCommand('copy');
+//     textarea.remove();
+// }
+
+// const text = document.getElementById('text');
+// const hover_text = document.getElementById('hover-text');
+// const change_text = document.getElementById('change-text');
+
+// text.addEventListener('click', copyClipboard);
+
+// text.addEventListener('mouseover', function () {
+//     hover_text.style.opacity = '1';
+// })
+
+// text.addEventListener('mouseout', function () {
+//     hover_text.style.opacity = '0';
+//     change_text.style.opacity = '0';
+// })
+
+// text.addEventListener('click', function () {
+//     hover_text.style.opacity = '0';
+//     change_text.style.opacity = '1';
+// })
+
+
+// menu icon
 const logoButton = document.getElementById('logo');
 
-const contactContainer = document.getElementById('contact-container');
 const mainContainer = document.getElementById('main-container');
+const menuContainer = document.getElementById('menu-container');
+const footerContainer = document.getElementById('footer-container');
 
-contactButton.addEventListener('click', function () {
+const menuIcon = document.getElementsByClassName('menu-icon')[0];
+const menu_top = document.getElementsByClassName('menu-top')[0];
+const menu_middle = document.getElementsByClassName('menu-middle')[0];
+const menu_bottom = document.getElementsByClassName('menu-bottom')[0];
 
-    contactContainer.style.display = 'flex';
-    mainContainer.style.opacity = '0';
+const menuItems = document.querySelectorAll('#menu-container p');
+const menuImg = document.getElementById('menu-img');
 
-    setTimeout(() => {
-        mainContainer.style.display = 'none';
-        contactContainer.style.opacity = '1';
-        contactContainer.style.transition = 'opacity 0.7s ease-in-out';
-    }, 500)
-})
+let isOpen = false
 
-logoButton.addEventListener('click', function () {
+menuIcon.addEventListener('click', function () {
+    if (!isOpen) {
+        menu_top.style.transform = 'rotate(45deg)';
+        menu_top.style.transition = '0.5s';
 
-    mainContainer.style.display = 'flex';
-    contactContainer.style.opacity = '0';
+        menu_middle.style.opacity = '0';
+        menu_middle.style.transition = '0.5s';
 
-    setTimeout(() => {
-        contactContainer.style.display = 'none';
-        mainContainer.style.opacity = '1';
-        mainContainer.style.transition = 'opacity 0.7s ease-in-out';
-    }, 500);
-})
+        menu_bottom.style.transform = 'rotate(135deg)';
+        menu_bottom.style.transition = '0.5s';
 
-// 클릭 시 클립보드 복사 & 텍스트 바꿈
-function copyClipboard() {
-    const text = document.getElementById('text').textContent;
-    const textarea = document.createElement('textarea');
-    textarea.textContent = text;
+        logoButton.style.opacity = '0';
+        logoButton.style.pointerEvents = 'none';
+        logoButton.style.transition = 'all 0.5s ease';
 
-    document.body.append(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    textarea.remove();
-}
+        mainContainer.style.opacity = '0';
+        mainContainer.style.transition = 'opacity 0.5s ease-in-out';
 
-const text = document.getElementById('text');
-const hover_text = document.getElementById('hover-text');
-const change_text = document.getElementById('change-text');
+        setTimeout(() => {
+            menuContainer.style.display = 'flex';
+            mainContainer.style.display = 'none';
+            footerContainer.style.display = 'none';
 
-text.addEventListener('click', copyClipboard);
+            menuItems.forEach((item, index) => {
+                setTimeout(() => {
+                    item.classList.add('animate');
+                }, index * 100 + 100);
+            });
 
-text.addEventListener('mouseover', function () {
-    hover_text.style.opacity = '1';
-})
+            setTimeout(() => {
+                menuImg.classList.add('animate');
+            }, 100);
+        }, 300);
 
-text.addEventListener('mouseout', function () {
-    hover_text.style.opacity = '0';
-    change_text.style.opacity = '0';
-})
+        isOpen = true; // 열린 상태로 변경
 
-text.addEventListener('click', function () {
-    hover_text.style.opacity = '0';
-    change_text.style.opacity = '1';
+    } else {
+        isOpen = false; // 닫힌 상태로 변경
+
+        setTimeout(() => {
+            menu_top.style.transform = '';
+            menu_middle.style.opacity = '1';
+            menu_bottom.style.transform = '';
+
+            mainContainer.style.opacity = '1';
+
+            menuItems.forEach((item, index) => {
+                setTimeout(() => {
+                    item.classList.remove('animate');
+                }, (menuItems.length - index - 1) * 100);
+            });
+
+            setTimeout(() => {
+                logoButton.style.opacity = '1';
+                logoButton.style.pointerEvents = 'all';
+
+                menuContainer.style.display = 'none';
+                mainContainer.style.display = 'flex';
+                footerContainer.style.display = 'flex';
+            }, 500);
+
+            menuImg.classList.remove('animate');
+        }, 500);
+    }
 })
